8068e400724c9d662b63634ab830c9a084a96bec
Sushant Shrestha <sushant.sht@gmail.com>
2024-01-03 18:25:21 +0545

    First commit

---
+++ python1.py
@@ -0,0 +1,21 @@
+num = 1 + 2 * 3 / 4.0
+print(num)
+
+remainder = 11 % 3
+print(remainder)
+
+square = 3 ** 2
+cube = 2 ** 3
+print(square)
+print(cube)
+
+stri = "Hey" + " " + "Matt" 
+print(stri)
+
+msg = "Hello " * 8
+print(msg)
+
+name = "Kris"
+age = 20
+print("Hello, %s!" %name)
+print("%s is %d years old." %(name,age))
