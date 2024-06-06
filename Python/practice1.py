249ceaee5bcd13c6d282791f5c08f0c6384785a0
Sushant Shrestha <sushant.sht@gmail.com>
2024-06-06 19:43:02 +0545

    1st commit

---
+++ Python/practice1.py
@@ -0,0 +1,11 @@
+#Print Statement
+print('Hello World')
+
+#Variables
+failed_subjects="2"
+name='John'
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
+print(name + '  will need to redo ' + failed_subjects + '  courses.')
+name="Eric"
+print(name + '  is doing well in geography.')
