8b588b4268f3b0ba561314d5bf34a2db272c56ec
Sushant Shrestha <sushant.sht@gmail.com>
2024-06-09 22:02:33 +0545

    1 commit

---
+++ JavaScript/practice1.js
@@ -0,0 +1,13 @@
+console.log(23); //Prints 23
+
+//Prints out 5 to the console
+console.log(5);
+
+//Prints out the string inside the quotation
+console.log('Location of Codecademy headquarters: 575 Broadway, New York City');
+console.log(40);
+
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
