2e83028290661e8f631d7bc5a07afcf350d034c0
Sushant Shrestha <sushant.sht@gmail.com>
2024-06-10 21:54:42 +0545

    2 commit

--- JavaScript/practice1.js
+++ JavaScript/practice1.js
@@ -7,7 +7,31 @@ console.log(5);
 console.log('Location of Codecademy headquarters: 575 Broadway, New York City');
 console.log(40);
 
-console.log('JavaScript');
-console.log(2011);
-console.log('Woohoo! I love to code! #codecademy');
-console.log(20.49);
+//Data Types
+console.log('JavaScript'); //prints JavaScript
+console.log(2011); //prints 2011
+console.log('Woohoo! I love to code! #codecademy'); //prints the text inside the quotation
+console.log(20.49); //prints 20.49
+
+//Arithmetic Operators
+console.log(23 + 3.5); //addition operator
+console.log(2024 - 1969); //subtraction operator
+console.log(65 / 240); //division operator
+console.log(0.2708 * 100); //multiplication operator    
+
+//String Concatenation
+console.log('Hello' + 'World'); //prints HelloWorld (no space between two strings)
+console.log('Hello' + ' ' + 'World') //prints Hello World (space between two strings)
+
+//Properties
+console.log('Teaching the world how to code'.length); //Prints 30
+
+//Methods
+console.log('Codecademy'.toUpperCase()); //prints to uppercase
+console.log('    Remove whitespace   '.trim()); //Removes the leading and trailing white space from a string.
+
+//Built-in Objects
+console.log(Math.random() * 100); //.random() generates a number between 0 to 1 but multiplying it with 100 makes it between 0 to 100
+console.log(Math.floor(Math.random() * 100)); //.floor() Returns the greatest integer less than or equal to its numeric argument
+console.log(Math.ceil(43.8)); //.ceil() Returns the smallest integer greater than or equal to its numeric argument
+console.log(Number.isInteger(2017)); //.isInteger() Returns true if the value passed is an integer, false otherwise.
