78212ad5ced2f26381f2c7809179b29eb1fa7b76
Sushant Shrestha <sushant.sht@gmail.com>
2024-06-11 19:41:05 +0545

    modified

--- JavaScript/practice1.js
+++ JavaScript/practice1.js
@@ -35,3 +35,63 @@ console.log(Math.random() * 100); //.random() generates a number between 0 to 1 
 console.log(Math.floor(Math.random() * 100)); //.floor() Returns the greatest integer less than or equal to its numeric argument
 console.log(Math.ceil(43.8)); //.ceil() Returns the smallest integer greater than or equal to its numeric argument
 console.log(Number.isInteger(2017)); //.isInteger() Returns true if the value passed is an integer, false otherwise.
+
+//Create a Variable: var
+var favoriteFood = 'pizza'; //A string named pizza is assigned to the variable name favoriteFood.
+var numOfSlices = 8; //A number 8 is assigned to the variable name numOfSlices.
+console.log(favoriteFood); //prints out pizza
+console.log(numOfSlices); //prints 8
+
+//Create a Variable: let : It allows us to re-assign the value in the same variable
+let changeMe = true; //A Boolean value true is assigned to the variable name changeMe
+changeMe = false; //A Boolean value false is re-assigned to the variable name changeMe
+console.log(changeMe); //prints false
+
+//Create a Variable: const :It does not alloe to re-assign the value in the same variable
+const entree = 'Enchiladas'; //A string Enchiladas is assigned to the variable name entree
+console.log(entree); //prints Enchiladas
+//These above two returns errors
+//entree = 'Tacos' 
+//const testing;
+
+//Mathematical Assignment Operators
+let levelUp = 10; //10 is assigned to variable levelUp
+levelUp += 5; //increases the value of levelUp by 5
+
+let powerLevel = 9001; //9001 is assigned to variable powerLevel
+powerLevel -= 100; //decreases the value of powerLevel by 100
+
+let multiplyMe = 32; //32 is assigned to variable multiplyMe
+multiplyMe *= 11; //multiplies the value of multiplyMe by 11
+
+let quarterMe = 1152; //1152 is assigned to variable quarterMe
+quarterMe /= 4; //divides the value of quarterMe by 4
+
+console.log('The value of levelUp:', levelUp); //prints 15
+console.log('The value of powerLevel:', powerLevel); //prints 8901 
+console.log('The value of multiplyMe:', multiplyMe); //prints 352
+console.log('The value of quarterMe:', quarterMe); //prints 288
+
+//The Increment and Decrement Operator
+let gainedDollar = 3; //3 is assigned to gainedDollar
+gainedDollar ++; //increases the value of gainedDollar by 1
+console.log(gainedDollar); //prints 4
+
+let lostDollar = 50; //50 is assigned to lostDollar
+lostDollar --; //decreases the value of lostDollarr by 1
+console.log(lostDollar); //prints 49
+
+//String Concatenation with Variables
+let favoriteAnimal = 'Dog'; //A string 'Dog' is assigned to variable favoriteAnimal
+console.log('My favorite animal: ' + favoriteAnimal); //prints My favorite animal: Dog
+
+//String Interpolation :template literals use backticks ` and ${} to interpolate values into a string.
+let myName = 'Sushant'; //A string 'Sushant' is assigned to variable myName
+let myCity = 'Paris'; //A string 'Paris' is assigned to variable myCity
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`); //prints My name is Sushant. My favorite city is Paris.
+
+//typeof operator :It returns the datatypes of the variables
+let newVariable = 'Playing around with typeof.'; //A string is assigned to newVariable
+console.log(typeof newVariable); //prints string
+newVariable = 1; //1 is re-assigned to newvariable
+console.log(typeof newVariable); //prints number
