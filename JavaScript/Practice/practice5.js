da3f8de7f994ea871649af792717d17003024e42
Sushant Shrestha <sushant.sht@gmail.com>
2024-06-21 20:47:53 +0545

    arrays

---
+++ JavaScript/Practice/practice5.js
@@ -0,0 +1,8 @@
+// ARRAYS
+// Create an Array
+
+const hobbies = ['Playing and Watching Football' , 'Watching Movies', 'Learn to code'];
+
+console.log(hobbies);// prints [ 'Playing and Watching Football', 'Watching Movies', 'Learn to code' ]
+
+//Accessing Elements
